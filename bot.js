8470079706a147d0f79e369b726cdd7f8d5bdbda
OmarEssamer <43882047+OmarEssamer@users.noreply.github.com>
2019-02-08 07:37:22 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,15 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Wlc Back,ON`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
+A7MD.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم 
+ لو سمحت ممكن تدخل سيرفرى الجديد 
+                               [https://discord.gg/hXv8Xv3]
+**`) 
+}).catch(console.error)
+})
 
 client.login(process.env.BOT_TOKEN);
